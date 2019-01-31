5a8098c99ae2022c597c8a9fdfe8ffe98a24b1e0
trevorMasonParivedaSolutions <47116874+trevorMasonParivedaSolutions@users.noreply.github.com>
2019-01-31 14:42:04 -0600

    Timer and home modal format

--- happyHourWebApp/src/app/home-page/home-page.component.ts
+++ happyHourWebApp/src/app/home-page/home-page.component.ts
@@ -12,11 +12,24 @@ export class HomePageComponent implements OnInit {
   headcount = 0;
   modalVisible: boolean = false;
   nameInput: string = "";
-  @Input() attendees;
+  @Input() attendees;;
+
+  fridayMil() {
+    let today = new Date();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    let dateTime = date + ' ' + time;
+    let mill = (new Date).getTime();
+    let current = mill;
+    return (1549058400000 - 3600000) - current;
+  }
+  countdown = this.countdownFunc(this.fridayMil());
 
   constructor() {
     console.log('Constructor initialised');
-
+    setInterval(() => {
+      this.countdown = this.countdownFunc(this.fridayMil());
+    }, 1000);
   }
 
   toggleModal() {
@@ -32,6 +45,37 @@ export class HomePageComponent implements OnInit {
     this.headcount += 1;
     this.toggleModal();
   }
+
+
+  countdownFunc(fridayMil) {
+
+    var delim = ":";
+    var hours = Math.ceil(fridayMil / (1000 * 60 * 60) % 60);
+    var minutes = Math.floor(fridayMil / (1000 * 60) % 60);
+    var seconds = Math.floor(fridayMil / 1000 % 60);
+
+    // hours = hours < 10 ? '0' + hours : hours;
+    // minutes = minutes < 10 ? '0' + minutes : minutes;
+    // seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    return (hours) + delim + minutes + delim + seconds;
+
+  }
+
+
+  // countdownFunc(duration) {
+  //   var milliseconds = ((duration % 1000) / 100),
+
+  //     seconds = ((duration / 1000) % 60),
+  //     minutes = ((duration / (1000 * 60)) % 60),
+  //     hours = ((duration / (1000 * 60 * 60)) % 24);
+
+  //   hours = (hours < 10) ? parseInt("0" + hours) : hours;
+  //   minutes = (minutes < 10) ? parseInt("0" + minutes) : minutes;
+  //   seconds = (seconds < 10) ? parseInt("0" + seconds) : seconds;
+
+  //   return hours + ":" + minutes + ":" + seconds + "." + milliseconds;
+  // }
 
   ngOnInit() {
     setTimeout(() => {
